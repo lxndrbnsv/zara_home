8b892069a8ee85599de3c07b4114e68743d9d36f
Alexander Abnosov <abnosov@gmail.com>
2021-04-08 19:19:23 +0300

    output to file

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+import sys
+
 from utils import ReadCategories, ReadProducts, WriteResults
 from scraper import GetCategories, GetProductLinks, GetProductData
 
+
+sys.stdout = open("logs.log", "w")
+sys.stderr = open("logs.log", "w")
 
 if __name__ == "__main__":
     categories = ReadCategories().cat_dicts
