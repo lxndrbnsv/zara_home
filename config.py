122429ff0a55261747fe07550e7744df2730b41e
Alexander Abnosov <abnosov@gmail.com>
2021-04-08 19:28:17 +0300

    cfg

---
+++ config.py
@@ -0,0 +1,10 @@
+webdriver = dict(
+    path="./webdriver/chromedriver",
+)
+urls = dict(category="https://www.zarahome.com/es/en/")
+request_data = dict(
+    headers={
+        "User-agent": "Mozilla/5.0 (X11; Linux x86_64; rv:68.0) Gecko/20100101 Firefox/68.0"
+    },
+    user_agent="Mozilla/5.0 (X11; Linux x86_64; rv:68.0) Gecko/20100101 Firefox/68.0",
+)
